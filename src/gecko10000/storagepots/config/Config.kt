06c094436f88493cb8055c805a27455abf52b37d
gecko10000 <levtey@gmail.com>
2025-01-31 15:00:20 -0600

    Change default upgrade amount to 1

--- src/gecko10000/storagepots/config/Config.kt
+++ src/gecko10000/storagepots/config/Config.kt
@@ -11,7 +11,7 @@ data class Config(
     val autosaveIntervalSeconds: Int = 300,
     val defaultMaxAmount: Long = 1000,
     val defaultAutoUpgrade: Boolean = false,
-    val storageUpgradeAmount: Int = 10,
+    val storageUpgradeAmount: Int = 1,
     val hopperTransferAmount: Int = 1,
     val hopperTransferCooldown: Int = 8,
 )
